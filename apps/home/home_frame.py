5ebe1c963c1dd1ae9a9de05f38c40d49f9b599ac
Smtp272 <101472734+Smtp272@users.noreply.github.com>
2023-02-18 15:24:29 +0300

    Update btns from

--- apps/home/home_frame.py
+++ apps/home/home_frame.py
@@ -14,21 +14,21 @@ class HomeFrame(Frame):
         self.root.title("VIDMAN")
         
         self.main_home_label = CTkLabel(
-            self, text="Welcome", text_font=("Montserrat", 30, "bold"))
+            self, text="Welcome", font=Constants.title_font)
         self.main_home_label.pack(pady=(0, 10))
 
         self.home_description_label = CTkLabel(self,
                                           text=f"Please select service below:",
-                                          text_font=("Montserrat", 15), wraplength=900)
+                                          font=Constants.instuction_font, wraplength=900)
         self.home_description_label.pack(pady=(0, 10))
 
-        self.mp4_convert_btn = CTkButton(self, text="Convert video to audio", fg_color=Constants.grey,
+        self.mp4_convert_btn = CTkButton(self, text="Convert video to audio", **Constants.btn_colour,
                                           command=lambda:open_command(mp4Frame))
         self.mp4_convert_btn.pack(pady=(0, 10))
-        self.yt_download_btn = CTkButton(self, text="Yt Download", fg_color=Constants.grey,
-                                          command=lambda:open_command(yTFrame))
+        self.yt_download_btn = CTkButton(self, text="Yt Download",**Constants.btn_colour,
+                                          command=lambda:open_command(yTFrame),)
         self.yt_download_btn.pack(pady=(0, 10))
-        self.clipper_btn = CTkButton(self, text="Clipper", fg_color=Constants.grey,
+        self.clipper_btn = CTkButton(self, text="Clipper", **Constants.btn_colour,
                                           command=lambda:open_command(clipperFrame))
         self.clipper_btn.pack(pady=(0, 10))
 
