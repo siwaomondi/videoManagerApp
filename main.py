f9446016ffaf979e6ad3b2a45c12faee8f97b5b1
Smtp272 <101472734+Smtp272@users.noreply.github.com>
2023-02-20 13:17:09 +0300

    Add output.txt file for sys.stdout to  write to

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from ui import VideoManager
-
+import sys
+output = open("output.txt", "wt")
+sys.stdout = output
+sys.stderr = output
 if __name__ == "__main__":
     app = VideoManager()
+output.close()
